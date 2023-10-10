89247557e1317e561d70f3d5ea2b2e7db591bc99
Dopadofull <melquistar@gmail.com>
2023-10-10 19:43:48 -0300

    cambie el logo de las paginas

--- src/layout/components/NavLogo.tsx
+++ src/layout/components/NavLogo.tsx
@@ -1,5 +1,18 @@
 import { Box } from "@mui/material";
+import {useRouter} from "next/router";
 
-const NavLogo = () => <Box>NJS</Box>;
+const NavLogo = () => {
+    const ubi = useRouter
+    return {
+        <>
+        {ubi.pathname === "/" && <Box>Home</Box>};
+        {ubi.pathname === "/login" && <Box>Login</Box>};
+        {ubi.pathname === "/homepage" && <Box>Bienvenido</Box>};
+        </>
+
+    }
+
+
+}
 
 export default NavLogo;
