f40c42f134aee1a2cd2ca32fa87a650ff5a9b64f
miguelo <md9914461@gmail.com>
2023-10-24 18:06:03 -0300

    aparece el nombre en que pagina estas pocisionado en el navlogo pero ahora hay un nuevo problema

--- src/layout/components/NavLogo.tsx
+++ src/layout/components/NavLogo.tsx
@@ -2,15 +2,15 @@ import { Box } from "@mui/material";
 import {useRouter} from "next/router";
 
 const NavLogo = () => {
-    const ubi = useRouter
-    return {
+    const ubi = useRouter()
+    return (
         <>
         {ubi.pathname === "/" && <Box>Home</Box>};
         {ubi.pathname === "/login" && <Box>Login</Box>};
         {ubi.pathname === "/homepage" && <Box>Bienvenido</Box>};
-        </>
+        </> 
 
-    }
+    )
 
 
 }
